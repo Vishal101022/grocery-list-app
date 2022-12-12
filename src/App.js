b27b0ca2308cc9d87ca8d423db1e5edbbc6da840
vishal101022 <raivishal939@gmail.com>
2022-12-12 20:20:49 +0530

    removed excessive  code

--- src/App.js
+++ src/App.js
@@ -1,13 +1,73 @@
-import Header from './Header'; 
-import Content from './Content';
-import Footer from './Footer'
+import Header from "./Header";
+import Content from "./Content";
+import Footer from "./Footer";
+import AddItems from "./AddItems";
+import SearchItems from "./SearchItems";
+import { useState } from "react";
 
 function App() {
+  // 3 hooks for each functions
+  const [items, setItems] = useState(
+    //retrive current localStorage data
+    JSON.parse(localStorage.getItem("grocerylist"))
+  );
+  const [newItem, setNewItem] = useState("");
+  const [search, setSearch] = useState("");
+
+  const setAndSaveItems = (newItems) => {
+    setItems(newItems); // updating data
+    // save updated data into localStorage
+    localStorage.setItem("grocerylist", JSON.stringify(newItems));
+  };
+
+  const addItem = (item) => {
+    // add new item at end of list
+    const id = items.length ? items[items.length - 1].id + 1 : 1;
+    // creating object
+    const myNewItem = { id, checked: false, item };
+    // merge old items with new items
+    const listItems = [...items, myNewItem];
+    setAndSaveItems(listItems);
+  };
+
+  const handleCheck = (id) => {
+    const listItems = items.map((item) =>
+      item.id === id ? { ...item, checked: !item.checked } : item
+    );
+    setAndSaveItems(listItems);
+  };
+
+  const handleDelete = (id) => {
+    const listItems = items.filter((item) => item.id !== id);
+    setAndSaveItems(listItems);
+  };
+
+  const handleSubmit = (e) => {
+    // prevent default submit
+    e.preventDefault();
+    if (!newItem) return;
+    addItem(newItem);
+    // clear submit input after add newitems
+    setNewItem("");
+  };
+
   return (
-    <div className="App">
-      <Header />
-      <Content />
-      <Footer />
+    <div className='App'>
+      <Header title='Grocery List' />
+      <AddItems
+        newItem={newItem}
+        setNewItem={setNewItem}
+        handleSubmit={handleSubmit}
+      />
+      <SearchItems search={search} setSearch={setSearch} />
+      <Content
+        items={items.filter((item) =>
+          item.item.toLowerCase().includes(search.toLowerCase())
+        )}
+        handleCheck={handleCheck}
+        handleDelete={handleDelete}
+      />
+      <Footer length={items.length} />
     </div>
   );
 }
--- src/Content.js
+++ src/Content.js
@@ -1,68 +1,16 @@
-import { useState } from "react";
-import { FaTrash } from "react-icons/fa";
+import ItemList from "./ItemList";
 
-const Content = () => {
-  // hooks
-  const [items, setItems] = useState([
-    {
-      id: 1,
-      checked: false,
-      item: "salt 2 packet",
-    },
-    {
-      id: 2,
-      checked: false,
-      item: "5 kilo rice",
-    },
-    {
-      id: 3,
-      checked: false,
-      item: "2 eggs",
-    },
-  ]);
-
-  const checkHandler = (id) => {
-    // declarative approach for checkbox
-    const listItems = items.map((item) =>
-      item.id === id ? { ...item, checked: !item.checked } : item
-    );
-    setItems(listItems);
-    // storing data even if browser close
-    localStorage.setItem("grocerylist", JSON.stringify(listItems));
-  };
-
-  const deleteHandler = (id) => {
-    const listItems = items.filter((item) => item.id !== id);
-    setItems(listItems);
-    localStorage.setItem("grocerylist", JSON.stringify(listItems));
-  };
-
+const Content = ({ items, handleCheck, handleDelete }) => {
   return (
     <main>
-      {items.length ? ( // condition for checking empty_list
-        <ul>
-          {items.map((item) => (
-            <li className='item' key={item.id}>
-              <input
-                type='checkbox'
-                onChange={() => checkHandler(item.id)}
-                checked={item.checked}
-              />
-
-              <label
-                // line-through when item is checked
-                style={
-                  item.checked ? { textDecoration: "line-through" } : null
-                }>
-                {item.item}
-              </label>
-
-              <FaTrash onClick={() => deleteHandler(item.id)} role='button' />
-            </li>
-          ))}
-        </ul>
+      {items.length ? (
+        <ItemList
+          items={items}
+          handleCheck={handleCheck}
+          handleDelete={handleDelete}
+        />
       ) : (
-        <p style={{ margin: "2rem" }}>List is Empty</p>
+        <p style={{ margin: "2rem" }}>Your list is empty.</p>
       )}
     </main>
   );

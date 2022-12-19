044ec5dd58a956c62a36677eb7fd55803232dc6d
vishal101022 <raivishal939@gmail.com>
2022-12-19 21:13:52 +0530

    added logo

--- src/Header.js
+++ src/Header.js
@@ -1,7 +1,9 @@
+import logo from "./asset/logo.svg";
 
 function Header(props) {
   return (
     <header>
+      <img id='logo' src={logo} alt='logo' />
       <h1>{props.title}</h1>
     </header>
   );
